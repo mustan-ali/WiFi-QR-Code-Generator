bb547983c6e256fc03254b7002582c47ab2be0b3
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2023-10-22 15:26:22 +0500

    added download functionality

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const generateButton = document.querySelector('button');
+const generateButton = document.getElementById('generate');
+const downloadButton = document.getElementById('download');
 
 generateButton.addEventListener('click', () => {
     generateButton.innerText = "Generating QR Code";
-    
+
     const ssidInput = document.getElementById('ssid');
     const passInput = document.getElementById('pass');
     const encryptionSelect = document.getElementById('encryption');
@@ -20,6 +21,23 @@ generateButton.addEventListener('click', () => {
     qrImg.addEventListener("load", () => {
         generateButton.innerText = "Generate QR Code";
         document.querySelector('.wrapper').classList.add("active");
+        downloadButton.style.display = "block";
     });
+});
 
+downloadButton.addEventListener('click', () => {
+    fetch(document.querySelector('.qr-code img').src)
+        .then(response => response.blob())
+        .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'WiFi-QR-Code.png';
+
+            document.body.appendChild(a);
+            a.click();
+
+            window.URL.revokeObjectURL(url);
+        });
 });
